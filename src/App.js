3a63ed7453e6c31f76ce87d471caa3d1e86b461a
176907347234 <marvin.abisrror@globant.com-at-176907347234>
2021-02-23 22:07:41 -0500

    react contacts first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI';
+
 
 class App extends Component {
+
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts: contacts
+        }))
+      })
+  }
+
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter(f => {
+        return f.id !== contact.id;
+      })
+    }))
+    ContactsAPI.remove(contact);
+  };
+
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts 
+          onDeleteContact={this.removeContact}
+          contacts={this.state.contacts}/>
       </div>
     );
   }

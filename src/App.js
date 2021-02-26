125fa56956c3d24249a2644c53f13054ccdbc7cf
176907347234 <marvin.abisrror@globant.com-at-176907347234>
2021-02-26 07:16:05 -0500

    route component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI';
-
+import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom';
 
 class App extends Component {
 
@@ -30,12 +31,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts 
+        <Route exact path="/" render={() => (
+          <ListContacts 
           onDeleteContact={this.removeContact}
-          contacts={this.state.contacts}/>
+          contacts={this.state.contacts}
+          onNavigate={() => {
+            this.setState(() => ({
+              screen: 'create'
+            }))
+          }}
+          />
+        )} />
+        <Route path="/create" component={CreateContact} />
       </div>
-    );
-  }
+    )}
 }
 
 export default App;
